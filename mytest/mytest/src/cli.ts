681f907cb40eded8187d06568273642d685b1510
jongwhan lee <jonghwan@crypto.com>
2021-07-06 15:14:49 +0800

    add ethers

--- mytest/mytest/src/cli.ts
+++ mytest/mytest/src/cli.ts
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
-import { foo } from './main'
+import { ethers } from "ethers"
 
-foo()
+
+async function test() {
+    const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
+    const b = await provider.getBlockNumber();
+    console.log(`${b}`)
+    console.log("OK")
+}
+
+test();
