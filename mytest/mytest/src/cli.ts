e55c211e48bd98f6cc6305b073da25655ad42ed4
jongwhan lee <jonghwan@crypto.com>
2021-07-06 16:41:39 +0800

    get balance

--- mytest/mytest/src/cli.ts
+++ mytest/mytest/src/cli.ts
@@ -6,7 +6,9 @@ async function test() {
     const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
     const b = await provider.getBlockNumber();
     console.log(`${b}`)
+    const balance = await provider.getBalance("0x59E9A2D1F17E1970BF3843F11CE0BB7419E48D43")
     console.log("OK")
+    console.log(`balance ${balance}`)
 }
 
 test();
