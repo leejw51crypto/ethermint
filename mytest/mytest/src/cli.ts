605d66e453b7430eff54467f17da437b6b651dd8
jongwhan lee <jonghwan@crypto.com>
2021-07-12 11:22:52 +0800

    Update cli.ts

--- mytest/mytest/src/cli.ts
+++ mytest/mytest/src/cli.ts
@@ -13,6 +13,7 @@ async function getWallet(index: number): Promise<ethers.Wallet> {
     let w = walletMnemonic.connect(provider)
     return w
 }
+
 async function getAddress(index: number): Promise<string> {
     let walletMnemonic = await getWallet(index)
     const myaddress = await walletMnemonic.getAddress()
@@ -24,6 +25,7 @@ async function getBalance(myaddress: string): Promise<string> {
     const mybalance = await provider.getBalance(myaddress)
     return mybalance.toString()
 }
+
 async function getBlockNumber(): Promise<string> {
     const provider = new ethers.providers.JsonRpcProvider(g_server);
     const b = await provider.getBlockNumber();
@@ -39,50 +41,30 @@ async function sendTx(fromaddr: string, toaddr: string): Promise<string> {
         value: ethers.utils.parseEther("1.0")
     });
     console.log(`tx ${JSON.stringify(tx)}`)
-
-
-    return ""
+    return "OK"
 }
-async function test2() {
-
+async function checkBasic() {
     const myaddress = await getAddress(0)
     const myaddress2 = await getAddress(1)
-
     const b = await getBlockNumber();
     console.log(`${b}`)
     const balance = await getBalance(myaddress)
     const balance2 = await getBalance(myaddress2)
     console.log(`${myaddress} balance ${balance}`)
     console.log(`${myaddress2} balance ${balance2}`)
-
     await sendTx(myaddress, myaddress2)
-    //const v1 = ethers.utils.parseEther("1.0")
-    //console.log(`v =${v1}`)
 
 }
 
 async function createContract(): Promise<string> {
     const myaddress = await getAddress(0)
     const balance = await getBalance(myaddress)
-    console.log("Hello World")
-    console.log(`${myaddress} balance ${balance}`)
-
     const contractByteCode = fs.readFileSync('hello_sol_Hello.bin', 'utf-8')
-    console.log(`binary ${contractByteCode}`)
-
     const contractAbi = JSON.parse(fs.readFileSync('hello_sol_Hello.abi', 'utf-8'))
-    console.log(`abi ${contractAbi}`)
-
     const provider = new ethers.providers.JsonRpcProvider(g_server);
     const signer = provider.getSigner()
-    console.log(`address ${await signer.getAddress()}`)
     const factory = new ContractFactory(contractAbi, contractByteCode, signer)
-
-    // If your contract requires constructor args, you can specify them here
     const contract = await factory.deploy()
-
-    console.log(contract)
-    // console.log(contract.deployTransaction)
     return contract.address
 
 }
@@ -103,16 +85,12 @@ async function processContract(contractAddress: string) {
     let contractWithSigner = contractInstance.connect(wallet);
     let tx = await contractWithSigner.store(ethers.BigNumber.from("0x15"))
     console.log(tx)
-
-
     currentValue = await contractInstance.retrieve();
     console.log(currentValue);
 
 }
 
-
 async function run() {
-
     let contractAddress = await createContract()
     console.log(`contract address ${contractAddress}`)
     processContract(contractAddress)
