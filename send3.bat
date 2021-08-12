836af46b846638037a5b0bf660af2fe36ee44187
jongwhan lee <jonghwan@crypto.com>
2021-08-12 19:07:25 +0900

    working

--- send3.bat
+++ send3.bat
@@ -3,13 +3,12 @@ set CLI=ethermintd
 set KEYRING=test
 set NODE=tcp://127.0.0.1:26657
 
-set K1=eth189up9vxymn2lcmuldf2wjp84j0994uv76e47tr
+set K1=eth1s28vg4ked3aeh6upsxc0l9jt85s7dq9pz4rhan
 set S1=eth1fzep9fc7hweq9a706x4vacardl0zl0z3rq3ls8
 set S2=eth1znmmtmaw8v0vucxtdtq2e3nnvl3v9c8c7z0mwa
 
-set FROM=%K1%
-set TO=%S1%
+set FROM=%S1%
+set TO=eth1xjfdas234fshncflwa0dyjgc23u08k9dqqyx2l
 
-rem set AMOUNT=1200000000000000aphoton
-set AMOUNT=1002000000000000000001aphoton
+set AMOUNT=12121000000000000001aphoton
 %CLI% tx bank  send %FROM% %TO% %AMOUNT% --chain-id %CHAINID% --keyring-backend %KEYRING%  --fees 20aphoton
