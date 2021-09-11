e93248f1fa91c1760e5a07eb83007ad4e6c7f973
jongwhan <jonghwan@crypto.com>
2021-09-11 11:59:50 +0900

    OK2

--- mycro/cronos/setup.sh
+++ mycro/cronos/setup.sh
@@ -1,6 +1,6 @@
 
 export KEY="mykey"
-export CHAINID="pearmint_8000-6"
+export CHAINID="cronos_777-1"
 export MONIKER="localtestnet"
 export KEYRING="test"
 export KEYALGO="eth_secp256k1"
@@ -8,14 +8,14 @@ export LOGLEVEL="info"
 # to trace evm
 export TRACE="--trace"
 #export TRACE=""
-export CHAINHOME=$HOME/.$CHAINID
+export CHAINHOME=/run/user/1000/pytest-of-leejw51/pytest-10/cronos0/cronos_777-1/node0
 
 export ETHCONFIG=$CHAINHOME/config/config.toml
 export GENESIS=$CHAINHOME/config/genesis.json
 export TMPGENESIS=$CHAINHOME/config/tmp_genesis.json
 export APPCONFIG=$CHAINHOME/config/app.toml
 export CLIENTCONFIG=$CHAINHOME/config/client.toml
-export DENOM=apear
+export DENOM=basetcro
 #echo $GENESIS
 #echo $TMPGENESIS
 #echo $ETHCONFIG
@@ -30,7 +30,7 @@ export ETHPORT1=8536
 export COSMOSPORT0=26646
 export COSMOSPORT1=26647
 export CLI=ethermintd
-export NODE=tcp://127.0.0.1:26647
+export NODE=tcp://127.0.0.1:26707
 $CLI keys list --keyring-backend $KEYRING --home $CHAINHOME
 #echo 'HOME='$CHAINHOME
 
