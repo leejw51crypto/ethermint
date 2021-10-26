53cc5f18c5f8e6623c1b781328940877fbbb4cb2
jongwhan <jonghwan@crypto.com>
2021-10-26 18:05:53 +0900

    space ready

--- rpc/ethereum/namespaces/eth/api.go
+++ rpc/ethereum/namespaces/eth/api.go
@@ -214,7 +214,35 @@ func (e *PublicAPI) MaxPriorityFeePerGas() (*hexutil.Big, error) {
 func (e *PublicAPI) FeeHistory(blockCount rpc.DecimalOrHex, lastBlock rpc.BlockNumber, rewardPercentiles []float64) (*rpctypes.FeeHistoryResult, error) {
 	e.logger.Debug("eth_feeHistory")
 
-	return nil, fmt.Errorf("eth_feeHistory not implemented")
+	var blockend int64 = int64(lastBlock)
+
+	if blockend <= 0 {
+		blockNumber, err := e.BlockNumber()
+		if err != nil {
+			return nil, err
+		}
+		blockend = int64(blockNumber)
+	}
+	var blockstart int64 = blockend - int64(blockCount)
+	var blockcount int64 = blockend - blockstart
+
+	var OldestBlock *hexutil.Big = (*hexutil.Big)(big.NewInt(blockstart))
+	var Reward [][]*hexutil.Big = make([][]*hexutil.Big, blockcount)
+	var rewardcount = len(rewardPercentiles)
+	for i := 0; i < int(blockCount); i++ {
+		Reward[i] = make([]*hexutil.Big, rewardcount)
+	}
+
+	var BaseFee []*hexutil.Big = make([]*hexutil.Big, blockcount)
+	var GasUsedRatio []float64 = make([]float64, blockcount)
+
+	feeHistory := rpctypes.FeeHistoryResult{
+		OldestBlock:  OldestBlock,
+		Reward:       Reward,
+		BaseFee:      BaseFee,
+		GasUsedRatio: GasUsedRatio}
+
+	return &feeHistory, fmt.Errorf("eth_feeHistory not implemented")
 }
 
 // Accounts returns the list of accounts available to this node.
