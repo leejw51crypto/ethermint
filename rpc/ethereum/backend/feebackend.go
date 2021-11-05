a3704886988a1240a9f700a5cb22e0f48b04afe8
jongwhan <jonghwan@crypto.com>
2021-11-05 18:21:26 +0900

    reward working
    fix panic
    
    value correct
    
    remove debugging log
    
    tidy up

--- rpc/ethereum/backend/feebackend.go
+++ rpc/ethereum/backend/feebackend.go
@@ -1,8 +1,6 @@
 package backend
 
 import (
-	"encoding/json"
-	"fmt"
 	"math/big"
 	"sort"
 	"strconv"
@@ -31,128 +29,83 @@ func (s sortGasAndReward) Less(i, j int) bool {
 }
 
 func (e *EVMBackend) processBlock(
-	tendermintblock *tmrpctypes.ResultBlock,
-	block *map[string]interface{}, rewardPercentiles []float64, blockresult *tmrpctypes.ResultBlockResults, onefeehistory *OneFeeHistory) error {
+	tendermintBlock *tmrpctypes.ResultBlock,
+	ethBlock *map[string]interface{}, rewardPercentiles []float64, tendermintBlockResult *tmrpctypes.ResultBlockResults, targetOneFeeHistory *OneFeeHistory) error {
 
-	height := tendermintblock.Block.Height
-	e.logger.Debug("processBlock #################")
-	e.logger.Debug("height {} #########", height)
-	json, jsonerr := json.Marshal(block)
-	if jsonerr != nil {
-		return jsonerr
-	}
-	e.logger.Debug(string(json))
-	basefee, err := e.BaseFee(height)
+	blockHeight := tendermintBlock.Block.Height
+	blockBaseFee, err := e.BaseFee(blockHeight)
 	if err != nil {
 		return err
 	}
 
 	// set basefee
-	onefeehistory.BaseFee = basefee
+	targetOneFeeHistory.BaseFee = blockBaseFee
 
 	// set gasused ratio
-	var gasLimit2 = (*block)["gasLimit"].(hexutil.Uint64)
-	var gasUsed4 = (*block)["gasUsed"].(*hexutil.Big)
-	var gasUsed3 = gasUsed4.ToInt().String()
-	e.logger.Debug("gasUsed3 {}", gasUsed3)
-	gasUsed2, _ := strconv.ParseFloat(gasUsed3, 64)
-	e.logger.Debug("gasLimit {}", gasLimit2)
-	e.logger.Debug("gasUsed {}", gasUsed2)
-	var gasusedratio float64 = 0
-	if gasLimit2 > 0 {
-		gasusedratio = float64(gasUsed2) / float64(gasLimit2)
+	var gasLimitUint64 = (*ethBlock)["gasLimit"].(hexutil.Uint64)
+	var gasUsedBig = (*ethBlock)["gasUsed"].(*hexutil.Big)
+	var gasUsedString = gasUsedBig.ToInt().String()
+	gasusedfloat, _ := strconv.ParseFloat(gasUsedString, 64)
+	var gasUsedRatio float64 = 0
+	if gasLimitUint64 > 0 {
+		gasUsedRatio = float64(gasusedfloat) / float64(gasLimitUint64)
 	}
-	var blockgasused = gasUsed2
+	var blockGasUsed = gasusedfloat
+	targetOneFeeHistory.GasUsed = gasUsedRatio
 
-	onefeehistory.GasUsed = gasusedratio
-
-	var rewardcount = len(rewardPercentiles)
-	onefeehistory.Reward = make([]*big.Int, rewardcount)
-	for i := 0; i < rewardcount; i++ {
-		onefeehistory.Reward[i] = big.NewInt(2000)
+	var rewardCount = len(rewardPercentiles)
+	targetOneFeeHistory.Reward = make([]*big.Int, rewardCount)
+	for i := 0; i < rewardCount; i++ {
+		targetOneFeeHistory.Reward[i] = big.NewInt(2000)
 	}
 
-	// check txs
-	txs := tendermintblock.Block.Txs
-	txresults := blockresult.TxsResults
-	txcount := len(txs)
+	// check tendermintTxs
+	tendermintTxs := tendermintBlock.Block.Txs
+	tendermintTxResults := tendermintBlockResult.TxsResults
+	tendermintTxCount := len(tendermintTxs)
+	sorter := make(sortGasAndReward, tendermintTxCount)
 
-	/*
-			sorter := make(sortGasAndReward, len(bf.block.Transactions()))
-		for i, tx := range bf.block.Transactions() {
-			reward, _ := tx.EffectiveGasTip(bf.block.BaseFee())
-			sorter[i] = txGasAndReward{gasUsed: bf.receipts[i].GasUsed, reward: reward}
-		}
-		sort.Sort(sorter)
+	for i := 0; i < tendermintTxCount; i++ {
+		eachTendermintTx := tendermintTxs[i]
+		eachTendermintTxResult := tendermintTxResults[i]
 
-		var txIndex int
-		sumGasUsed := sorter[0].gasUsed
-
-		for i, p := range percentiles {
-			thresholdGasUsed := uint64(float64(bf.block.GasUsed()) * p / 100)
-			for sumGasUsed < thresholdGasUsed && txIndex < len(bf.block.Transactions())-1 {
-				txIndex++
-				sumGasUsed += sorter[txIndex].gasUsed
-			}
-			bf.results.reward[i] = sorter[txIndex].reward
-		}
-	*/
-
-	sorter := make(sortGasAndReward, txcount)
-
-	for i := 0; i < txcount; i++ {
-		txBz := txs[i]
-		txresult := txresults[i]
-
-		tx, err := e.clientCtx.TxConfig.TxDecoder()(txBz)
+		tx, err := e.clientCtx.TxConfig.TxDecoder()(eachTendermintTx)
 		if err != nil {
-			e.logger.Debug("failed to decode transaction in block", "height", height, "error", err.Error())
+			e.logger.Debug("failed to decode transaction in block", "height", blockHeight, "error", err.Error())
 			continue
 		}
-		gasused := uint64(txresult.GasUsed)
+		txGasUsed := uint64(eachTendermintTxResult.GasUsed)
 		for _, msg := range tx.GetMsgs() {
 			ethMsg, ok := msg.(*evmtypes.MsgEthereumTx)
 			if !ok {
 				continue
 			}
-
 			tx := ethMsg.AsTransaction()
-			reward := tx.EffectiveGasTipValue(basefee)
-			sorter[i] = txGasAndReward{gasUsed: gasused, reward: reward}
-			fmt.Printf("reward %v  gas used %v", reward, gasused)
-
-			hash := tx.Hash()
-			fmt.Printf("tx=%v hash=%v", tx, hash)
+			reward := tx.EffectiveGasTipValue(blockBaseFee)
+			sorter[i] = txGasAndReward{gasUsed: txGasUsed, reward: reward}
 			break
 		}
 	}
 	sort.Sort(sorter)
 
-	// print
-	for index, element := range sorter {
-		a := fmt.Sprintf("index= %d  gasused= %v   reward= %v", index, element.gasUsed, element.reward)
-		e.logger.Debug(a)
-	}
-
 	var txIndex int
-	sumGasUsed := sorter[0].gasUsed
+	sumGasUsed := uint64(0)
+	if len(sorter) > 0 {
+		sumGasUsed = sorter[0].gasUsed
+	}
 	for i, p := range rewardPercentiles {
-		thresholdGasUsed := uint64(float64(blockgasused) * p / 100)
-		for sumGasUsed < thresholdGasUsed && txIndex < txcount-1 {
-			// b := fmt.Sprintf("index %d  txindex %d  percent %f  thresholdGasUsed %d sumGasUsed %d", i, txIndex, p, thresholdGasUsed, sumGasUsed)
-			// e.logger.Debug(b)
+		thresholdGasUsed := uint64(float64(blockGasUsed) * p / 100)
+		for sumGasUsed < thresholdGasUsed && txIndex < tendermintTxCount-1 {
 			txIndex++
 			sumGasUsed += sorter[txIndex].gasUsed
 		}
-		b := fmt.Sprintf("index %d    percent %f  thresholdGasUsed %d sumGasUsed %d", i, p, thresholdGasUsed, sumGasUsed)
-		e.logger.Debug(b)
 
-		onefeehistory.Reward[i] = sorter[txIndex].reward
+		chosenReward := big.NewInt(0)
+		if 0 <= txIndex && txIndex < len(sorter) {
+			chosenReward = sorter[txIndex].reward
+		}
+		targetOneFeeHistory.Reward[i] = chosenReward
 	}
-
-	e.logger.Debug(fmt.Sprintf("gasLimit %d", gasLimit2))
-	e.logger.Debug(fmt.Sprintf("gasUsed %f ", gasUsed2))
-	e.logger.Debug(fmt.Sprintf("sumGasUsed %d ", sumGasUsed))
 
 	return nil
 }
@@ -163,61 +116,64 @@ type OneFeeHistory struct {
 	GasUsed float64
 }
 
-func (e *EVMBackend) FeeHistory(blockCount rpc.DecimalOrHex, lastBlock rpc.BlockNumber, rewardPercentiles []float64) (*rpctypes.FeeHistoryResult, error) {
-	e.logger.Debug("eth_feeHistory count {}   ", blockCount)
+func (e *EVMBackend) FeeHistory(userBlockCount rpc.DecimalOrHex, lastBlock rpc.BlockNumber, rewardPercentiles []float64) (*rpctypes.FeeHistoryResult, error) {
 
-	var blockend int64 = int64(lastBlock)
+	var blockEnd int64 = int64(lastBlock)
 
-	if blockend <= 0 {
+	if blockEnd <= 0 {
 		blockNumber, err := e.BlockNumber()
 		if err != nil {
 			return nil, err
 		}
-		blockend = int64(blockNumber)
+		blockEnd = int64(blockNumber)
 	}
-	var blockstart int64 = blockend - int64(blockCount)
-	var blockcount int64 = blockend - blockstart
+	var blockStart int64 = blockEnd - int64(userBlockCount)
+	if blockStart < 0 {
+		blockStart = 0
+	}
 
-	var OldestBlock *hexutil.Big = (*hexutil.Big)(big.NewInt(blockstart))
+	var blockCount int64 = blockEnd - blockStart
+
+	var OldestBlock *hexutil.Big = (*hexutil.Big)(big.NewInt(blockStart))
 
 	// prepare space
-	var Reward [][]*hexutil.Big = make([][]*hexutil.Big, blockcount)
+	var Reward [][]*hexutil.Big = make([][]*hexutil.Big, blockCount)
 	var rewardcount = len(rewardPercentiles)
 	for i := 0; i < int(blockCount); i++ {
 		Reward[i] = make([]*hexutil.Big, rewardcount)
 	}
-	var BaseFee []*hexutil.Big = make([]*hexutil.Big, blockcount)
-	var GasUsedRatio []float64 = make([]float64, blockcount)
+	var BaseFee []*hexutil.Big = make([]*hexutil.Big, blockCount)
+	var GasUsedRatio []float64 = make([]float64, blockCount)
 
 	// fetch block
-	for blockid := blockstart; blockid < blockend; blockid++ {
-		index := int32(blockid - blockstart)
+	for blockID := blockStart; blockID < blockEnd; blockID++ {
+		index := int32(blockID - blockStart)
 		// eth block
-		foundblock, err := e.GetBlockByNumber(rpctypes.BlockNumber(blockid), true)
+		ethBlock, err := e.GetBlockByNumber(rpctypes.BlockNumber(blockID), true)
 		if err != nil {
 			return nil, err
 		}
 
 		// tendermint block
-		tendermintblock, tenderminterr := e.GetTendermintBlockByNumber(rpctypes.BlockNumber(blockid))
+		tendermintblock, tenderminterr := e.GetTendermintBlockByNumber(rpctypes.BlockNumber(blockID))
 		if tenderminterr != nil {
 			return nil, err
 		}
 
-		// block result
-		foundblockresult, err := e.clientCtx.Client.BlockResults(e.ctx, &tendermintblock.Block.Height)
+		// tendermint block result
+		tendermintBlockResult, err := e.clientCtx.Client.BlockResults(e.ctx, &tendermintblock.Block.Height)
 		if err != nil {
 			e.logger.Debug("EthBlockFromTendermint block result not found", "height", tendermintblock.Block.Height, "error", err.Error())
 			return nil, err
 		}
 
 		var onefeehistory OneFeeHistory = OneFeeHistory{}
-		processerr := e.processBlock(tendermintblock, &foundblock, rewardPercentiles, foundblockresult, &onefeehistory)
+		processerr := e.processBlock(tendermintblock, &ethBlock, rewardPercentiles, tendermintBlockResult, &onefeehistory)
 		if processerr != nil {
 			return nil, processerr
 		}
 
-		// iterate
+		// copy
 		BaseFee[index] = (*hexutil.Big)(onefeehistory.BaseFee)
 		GasUsedRatio[index] = onefeehistory.GasUsed
 		for j := 0; j < rewardcount; j++ {
@@ -232,5 +188,4 @@ func (e *EVMBackend) FeeHistory(blockCount rpc.DecimalOrHex, lastBlock rpc.Block
 		BaseFee:      BaseFee,
 		GasUsedRatio: GasUsedRatio}
 	return &feeHistory, nil
-	//return &feeHistory, fmt.Errorf("eth_feeHistory not implemented")
 }
